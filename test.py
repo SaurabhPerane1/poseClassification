e39f20abaed330a13af4fa7f2cc553baeb124e56
SaurabhPerane1 <119008712+SaurabhPerane1@users.noreply.github.com>
2025-04-05 16:41:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,11 @@ from streamlit_webrtc import webrtc_streamer
 import av
 
 def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
+    try:    
+        img = frame.to_ndarray(format="bgr24")
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
+    except Exception as e:
+        print("Error in frame processing: ", e)
+        return None
 
 webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
