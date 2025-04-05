c9984040fd76bb10876d31759f3951ee51a6aeae
SaurabhPerane1 <119008712+SaurabhPerane1@users.noreply.github.com>
2025-04-05 16:23:22 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import av
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
