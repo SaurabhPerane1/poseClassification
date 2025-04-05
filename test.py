1b2a65347e9e6490e7be23d40429da066fa93029
SaurabhPerane1 <119008712+SaurabhPerane1@users.noreply.github.com>
2025-04-05 16:47:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import av
+import cv2
+import time
+
+last_frame_time = 0
+min_interval = 1/10
 
 def video_frame_callback(frame):
+    global last_frame_time
+    now = time.time()
+    if now-last_frame_time<min_interval:
+        return None
+    last_frame_time = now
     try:    
         img = frame.to_ndarray(format="bgr24")
+        img = cv2.resize(img, (320, 240))
         return av.VideoFrame.from_ndarray(img, format="bgr24")
     except Exception as e:
         print("Error in frame processing: ", e)
