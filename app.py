59d0a3463949a393a087dd0383a39e1d6621e2a1
SaurabhPerane1 <119008712+SaurabhPerane1@users.noreply.github.com>
2025-04-05 15:55:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 import cv2
 import numpy as np
 import tensorflow as tf
 
-# Load model once
+pose_classes = ['Downdog', 'Goddess', 'Plank', 'Tree', 'Warrior2']
+
 @st.cache_resource
 def load_model():
     return tf.keras.models.load_model("yoga_pose_classifier1.keras")
 
 model = load_model()
-
-pose_classes = ['Downdog', 'Goddess', 'Plank', 'Tree', 'Warrior2']
 
 def preprocess_frame(frame):
     img = cv2.resize(frame, (150, 150))
@@ -18,35 +18,17 @@ def preprocess_frame(frame):
     img = np.expand_dims(img, axis=0)
     return img
 
-st.title("Pose Classification - Live Feed")
+class VideoProcessor(VideoProcessorBase):
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        input_img = preprocess_frame(img_rgb)
+        prediction = model.predict(input_img)
+        predicted_class = pose_classes[np.argmax(prediction)]
+        cv2.putText(img, f'Pose: {predicted_class}', (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        return img
 
-# Initialize session state
-if 'camera_active' not in st.session_state:
-    st.session_state.camera_active = False
+st.title("Yoga Pose Classifier - Live")
 
-start = st.button('Start Webcam')
-stop = st.button('Stop Webcam')
-
-FRAME_WINDOW = st.image([])
-
-if start:
-    st.session_state.camera_active = True
-
-if stop:
-    st.session_state.camera_active = False
-
-if st.session_state.camera_active:
-    camera = cv2.VideoCapture(0)
-    if not camera.isOpened():
-        st.error("Unable to access camera")
-    else:
-        ret, frame = camera.read()
-        if ret:
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            input_img = preprocess_frame(frame_rgb)
-            prediction = model.predict(input_img)
-            predicted_class = pose_classes[np.argmax(prediction)]
-            cv2.putText(frame_rgb, f'Pose: {predicted_class}', (10, 30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-            FRAME_WINDOW.image(frame_rgb)
-        camera.release()
+webrtc_streamer(key="pose", video_processor_factory=VideoProcessor)
